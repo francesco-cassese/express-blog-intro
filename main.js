4a4c79fbbd9b491d7091e437cc57f1df3a4852d6
Francesco Cassese <francescocassese9@gmail.com>
2026-05-21 14:34:36 +0200

    feat: inizializzato il server e scaricato express

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Ciao');
+import express from 'express';
+
+const app = express();
+const SERVER_PORT = process.env.SERVER_PORT;
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server in ascolto su ${SERVER_PORT}`);
+})
